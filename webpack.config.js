36b73d39592ab864c43f4e26a46c6a556afaf4d9
joma74 <dev-mails@gmx.net>
2019-02-11 00:12:48 +0100

    chore: aligned path aliase

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,7 @@ const webpack_aliase = {
   "@font": path.resolve(__dirname, "src/assets/fonts/"),
   "@home": path.resolve(__dirname),
   "@html": path.resolve(__dirname, "src/html/"),
+  "@icons": path.resolve(__dirname, "src/assets/icons/"),
   "@img": path.resolve(__dirname, "src/assets/img/"),
   "@js": path.resolve(__dirname, "src/js/"),
   "@svg": path.resolve(__dirname, "src/assets/svg/"),
