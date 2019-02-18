64535dacb605c22d6c3648cc671b5d47a1aa29b0
joma74 <dev-mails@gmx.net>
2019-02-19 00:50:32 +0100

    feat: started sticky header theme

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,11 @@
 import $ from "jquery"
 
-$(document).ready(function() {
+/**
+ * The $ aka $(document).ready() method offers a way to run JavaScript
+ * code as soon as the page's Document Object Model
+ * (DOM) becomes safe to manipulate.
+ */
+$(function() {
   console.log(`jquery version at ${$.fn.jquery}`)
   $("h1").click(function() {
     $(this).css("background-color", "#ff0000")
